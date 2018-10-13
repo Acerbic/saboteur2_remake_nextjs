bc4a0e6279282f7ea553e79053553d81176edc97
Anthony Cerbic <kuzzdra@gmail.com>
2018-10-13 05:05:45 +0300

    nice-ify menu links code

--- components/MenuLink.js
+++ components/MenuLink.js
@@ -6,23 +6,22 @@ const MenuLink = ({ children, router, href }) => {
     const active = router.pathname == href;
 
     return (
-        <>
+        <React.Fragment>
             <Link href={href}>
-                <a className={active ? "current": null }>{children}
-                    <style jsx>{`
-                        a  {
-                            color: #FFB90F;
-                            font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-                            text-decoration: none;
-                        }
-
-                        a.current {
-                            color: #FF4500;
-                        }
-                    `}</style>
-                </a>
+                <a className={active ? "current": null }>{children}</a>
             </Link>
-        </>
+            <style jsx>{`
+                a {
+                    color: #FFB90F;
+                    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+                    text-decoration: none;
+                }
+
+                a.current {
+                    color: #FF4500;
+                }
+            `}</style>
+        </React.Fragment>
     );
 }
 
