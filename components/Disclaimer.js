e24b49daee4dd3ae5f6dde23a3b5d30627537268
Anthony Cerbic <kuzzdra@gmail.com>
2018-10-28 23:29:59 +0300

    disclaimer

---
+++ components/Disclaimer.js
@@ -0,0 +1,17 @@
+export default () => (
+    <div id="disclaimer">
+        <div id="popup">
+            <div id="disclaimer-message">
+                <em>Важно!</em> Сайт, на котором вы сейчас находитесь &mdash; реплика сайта
+                <a href="http://www.saboteur2.ru">www.saboteur2.ru</a>, созданная как
+                упражнение в HTML5+CSS. Авторские права на статьи сайта и все 
+                визуальные решения принадлежат автору исходного сайта. Я 
+                всего лишь перевёл содержимое на современные веб технологии.
+                См. <a href="https://bitbucket.org/GlebVarenov/saboteur2_remake#readme">
+                проект</a> на Bitbucket.
+            </div>
+        </div>
+        <div id="corner"></div>
+        <div id="anchor">!</div>
+    </div>
+)
