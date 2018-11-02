7637879432b863483663982af26ad70207a8e6c0
Anthony Cerbic <kuzzdra@gmail.com>
2018-11-02 22:31:04 +0300

    fix Map and add Fullscreen button

--- components/ImageMap.js
+++ components/ImageMap.js
@@ -10,9 +10,12 @@ class ImageMap extends React.Component {
 
     componentDidMount() {
         import('./olMods')
-            .then( ({olMap, olView, ImageLayer, ImageStatic}) => {
+            .then(({ olMap, olView, ImageLayer, ImageStatic, defaultControls, FullScreen }) => {
                 const map = new olMap({
-                    target: 'map',
+                    target: 'ol-map',
+                    controls: defaultControls().extend([
+                        new FullScreen({ label: 'Fullscreen' })
+                    ]),
                     layers: [
                         new ImageLayer({
                             source: new ImageStatic({
@@ -34,12 +37,40 @@ class ImageMap extends React.Component {
                 });
                 this.setState({ map });
             })
-            .catch( e => console.log(e) )
+            .catch(e => console.log(e))
     }
 
-    render () {
+    render() {
         return (
-            <div id="map" style={ {maxHeight: '80vh'} }>
+            <div id="ol-container">
+                <div id="ol-map">
+                </div>
+                <style jsx>{`
+                    #ol-container {
+                        padding: 2em;
+                        width: 1001.3px;
+                        max-width: 90%;
+                        box-sizing: border-box;
+                        border: thin solid gray;
+                        margin: 0 auto;
+                        height: calc(100vh - 20em);
+                        /* this translate is an odd fix to an odd issue with
+                        OpenLayers library - when dimension of a container
+                        is ~just wrong~ (i.e. 1001.3px), there happens
+                        a rounding error and the image gets a bit distorted */
+                        transform: translateZ(0);
+                    }
+                    #ol-map {
+                        height: 100%;
+                    }
+                `}</style>
+                <style jsx global>{`
+                    button.ol-full-screen-false {
+                        font-weight: normal;
+                        width: unset;
+                        padding: 0 1em;
+                    }
+                `}</style>
             </div>
         );
     }
